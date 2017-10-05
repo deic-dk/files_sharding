25b93272ce211dea866ffd602feaa0616b8f8975
deltafunction <frederik@orellana.dk>
2017-10-05 15:00:26 +0200

    Security fixes.

--- ws/newfolder.php
+++ ws/newfolder.php
@@ -23,12 +23,18 @@ $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : '';
 $group = isset($_REQUEST['group']) ? $_REQUEST['group'] : '';
 $group_dir_owner = \OCP\USER::getUser();
 
-if($owner){
+
+if(!empty($owner)){
+	if(empty($user_id)){
+		$user_id = \OCP\USER::getUser();
+	}
+	\OC_Util::teardownFS();
 	\OC_User::setUserId($owner);
 	\OC_Util::setupFS($owner);
 	$group_dir_owner = $owner;
+
 }
-elseif($user_id && !\OCP\USER::getUser()){
+elseif($user_id && $user_id!=\OCP\USER::getUser()){
 	\OC_User::setUserId($user_id);
 	\OC_Util::setupFS($user_id);
 	$group_dir_owner = $user_id;
@@ -42,26 +48,28 @@ if(!empty($group) && !empty($group_dir_owner)){
 
 if($id){
 	$dir = \OC\Files\Filesystem::getPath($id);
-	\OCP\Util::writeLog('files_sharding', 'DIR: '.$dir.', PATH: '.$path.', ID: '.$id, \OC_Log::WARN);
+	\OCP\Util::writeLog('files_sharding', 'DIR: '.$dir.', ID: '.$id, \OC_Log::WARN);
 }
 
 $l10n = \OC_L10n::get('files');
 
 $result = array(
-	'success' 	=> false,
-	'data'		=> NULL
+	'success' => false,
+	'data' => NULL
 	);
 
 if(trim($foldername) === '') {
 	$result['data'] = array('message' => $l10n->t('Folder name cannot be empty.'));
+	restoreUser($user_id, $owner);
 	OCP\JSON::error($result);
-	myexit($user_id, $owner);
+	exit();
 }
 
 if(!OCP\Util::isValidFileName($foldername)) {
 	$result['data'] = array('message' => (string)$l10n->t("Invalid name, '\\', '/', '<', '>', ':', '\"', '|', '?' and '*' are not allowed."));
+	restoreUser($user_id, $owner);
 	OCP\JSON::error($result);
-	myexit($user_id, $owner);
+	exit();
 }
 
 if (!\OC\Files\Filesystem::file_exists($dir . '/')) {
@@ -69,8 +77,9 @@ if (!\OC\Files\Filesystem::file_exists($dir . '/')) {
 			'The target folder has been moved or deleted.'),
 			'code' => 'targetnotfound'
 		);
+	restoreUser($user_id, $owner);
 	OCP\JSON::error($result);
-	myexit($user_id, $owner);
+	exit();
 }
 
 //TODO why is stripslashes used on foldername here but not in newfile.php?
@@ -81,8 +90,9 @@ if (\OC\Files\Filesystem::file_exists($target)) {
 			'The name %s is already used in the folder %s. Please choose a different name.',
 			array($foldername, $dir))
 		);
+	restoreUser($user_id, $owner);
 	OCP\JSON::error($result);
-	myexit($user_id, $owner);
+	exit();
 }
 
 if(\OC\Files\Filesystem::mkdir($target)) {
@@ -93,18 +103,18 @@ if(\OC\Files\Filesystem::mkdir($target)) {
 	}
 	$meta = \OC\Files\Filesystem::getFileInfo($path);
 	$meta['type'] = 'dir'; // missing ?!
+	restoreUser($user_id, $owner);
 	OCP\JSON::success(array('data' => \OCA\Files\Helper::formatFileInfo($meta)));
-	myexit($user_id, $owner);
+	exit();
 }
 
-function myexit($user_id, $owner){
+function restoreUser($user_id, $owner){
 	if($user_id && $owner && $user_id != $owner){
 		// If not done, the user shared with will now be logged in as $owner
 		\OC_Util::teardownFS();
 		\OC_User::setUserId($user_id);
 		\OC_Util::setupFS($user_id);
 	}
-	exit();
 }
 
 OCP\JSON::error(array('data' => array( 'message' => $l10n->t('Error when creating the folder') )));
