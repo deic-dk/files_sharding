edc285d019c9a43bd7a4ca103adfd97298f62b30
deltafunction <frederik@orellana.dk>
2018-09-22 21:05:20 +0200

    Upload fix.

--- lib/lib_files_sharding.php
+++ lib/lib_files_sharding.php
@@ -2479,10 +2479,11 @@ class Lib {
 		else{
 			$dataDir = \OC_Config::getValue("datadirectory", \OC::$SERVERROOT . "/data");
 			$fullEndPath = $dataDir.'/'.
-			(empty($group)?\OCP\USER::getUser().'/':'/').
-			trim(\OC\Files\Filesystem::getRoot(), '/').'/'.trim($endPath, '/');
+				//(empty($group)?\OCP\USER::getUser().'/':'/').
+				\OCP\USER::getUser().'/'.
+				trim(\OC\Files\Filesystem::getRoot(), '/').'/'.trim($endPath, '/');
 			\OCP\Util::writeLog('files_sharding', 'Moving tmp file: '.$tmpFile.'->'.$dataDir.'->'.$endPath.'->'.
-					\OC\Files\Filesystem::getRoot().'->'.$$fullEndPath.':'.\OCP\USER::getUser(), \OC_Log::WARN);
+					\OC\Files\Filesystem::getRoot().'->'.$fullEndPath.':'.\OCP\USER::getUser(), \OC_Log::WARN);
 			$ret = rename($tmpFile, $fullEndPath);
 		}
 		
