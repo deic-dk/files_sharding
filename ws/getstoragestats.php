7e363c7d854959b1c0d923536349b81dbee78146
deltafunction <frederik@orellana.dk>
2018-12-03 12:58:57 +0100

    Admin usability

---
+++ ws/getstoragestats.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/../../../../../lib/base.php';
+
+if(!OCA\FilesSharding\Lib::checkIP()){
+	http_response_code(401);
+	exit;
+}
+
+$dir = !empty($_GET['dir'])?$_GET['dir']:'/';
+$owner = !empty($_GET['owner'])?$_GET['owner']:'';
+$id = !empty($_GET['id'])?$_GET['id']:'';
+$group = !empty($_GET['group'])?$_GET['group']:'';
+
+
+\OC_Util::setupFS();
+$ret = OCA\FilesSharding\Lib::buildFileStorageStatistics($dir, $owner, $id, $group);
+
+OCP\JSON::success(array('data' => $ret));
